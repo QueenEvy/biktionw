75329ad5fe922b245a249ae02f1164f2afb832e1
wquiceno1 <wquiceno@gmail.com>
2021-09-06 21:04:24 -0500

    Messages fixed2

--- check_availability.php
+++ check_availability.php
@@ -7,11 +7,11 @@ if(!empty($_POST["email"])) {
 		$count=mysqli_num_rows($result);
 if($count>0)
 {
-echo "<span style='color:red'> Email already exists .</span>";
+echo "<span style='color:red'> El correo ya existe .</span>";
  echo "<script>$('#submit').prop('disabled',true);</script>";
 } else{
 	
-	echo "<span style='color:green'> Email available for Registration .</span>";
+	echo "<span style='color:green'> Correo disponible. .</span>";
  echo "<script>$('#submit').prop('disabled',false);</script>";
 }
 }
